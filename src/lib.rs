89d3e5ea92735a0effe32748234cfe9176bf7dfa
huskyhoochu <dfg1499@gmail.com>
2023-04-22 12:07:09 +0900

    first impl markdown parsing algorithm

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,52 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
+use std::str::FromStr;
+
+use regex::Regex;
+
+#[derive(Debug, PartialEq)]
+pub enum MarkdownType {
+    H1,
+    H2,
+    H3,
+    P,
+    Strong,
+    Italic,
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+#[derive(Debug, PartialEq)]
+pub struct Markdown {
+    pub style: MarkdownType,
+    pub content: String,
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+impl FromStr for Markdown {
+    type Err = std::str::Utf8Error;
+
+    fn from_str(line: &str) -> Result<Self, Self::Err> {
+        let h1_regex = Regex::new(r"^#\s").unwrap();
+
+        if h1_regex.is_match(line) {
+            return Ok(Markdown {
+                style: MarkdownType::H1,
+                content: line.to_string(),
+            });
+        }
+
+        Ok(Markdown {
+            style: MarkdownType::P,
+            content: line.to_string(),
+        })
     }
+}
+
+pub fn render(origin_string: String) -> Vec<Markdown> {
+    origin_string
+        .split('\n')
+        .into_iter()
+        .map(|line| match Markdown::from_str(line) {
+            Ok(markdown) => markdown,
+            Err(_) => {
+                println!("not valid markdown");
+                return Markdown { style: MarkdownType::P, content: line.to_string() };
+            }
+        }).collect()
 }
---
+++ tests/lib-test.rs
@@ -0,0 +1,12 @@
+#[cfg(test)]
+mod tests {
+    use ipsae::{Markdown, MarkdownType};
+
+    #[test]
+    fn it_works() {
+        let origin_string = "# hello\nmy friend!".to_string();
+        let result = ipsae::render(origin_string);
+        assert_eq!(result.len(), 2);
+        assert_eq!(result[0], Markdown { style: MarkdownType::H1, content: "# hello".to_string() })
+    }
+}
